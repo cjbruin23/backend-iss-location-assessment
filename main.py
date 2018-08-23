68d53cbec61fb54f16252351ae1518089501b1cb
Colin Bruin <colinjb23@gmail.com>
2018-08-23 16:41:07 -0400

    finished with parts B and C

---
+++ main.py
@@ -0,0 +1,57 @@
+import requests
+import urllib
+import turtle
+import time
+
+def grab_astros():
+    response = requests.get('http://api.open-notify.org/astros.json')
+    data = response.json()
+    return data['people']
+
+def grab_coordinates():
+    response = requests.get('http://api.open-notify.org/iss-now.json')
+    data = response.json()
+    position = data['iss_position']
+    return position
+
+def turtle_setup():
+    window = turtle.Screen()
+    window.bgpic("map.gif")
+    window.addshape("iss.gif")
+    sk = turtle.Turtle()
+    sk2 = turtle.Turtle()
+    sk2.goto(-86, 32)
+    sk2.dot(5, "yellow")
+    sk.shape("iss.gif")
+    return sk
+
+def indy_data():
+    response = requests.get('http://api.open-notify.org/iss-pass.json', params={'lat': 39.82, 'lon': -86.172})
+    data = response.json()
+    first_time = data['response'][0]['risetime']
+    spec_time = time.ctime(first_time)
+    return spec_time
+
+    
+def main():
+    
+    people = grab_astros()
+    print 'Number of astronauts in space:', len(people)
+    for person in people:
+        print 'Name:', person['name']
+        print 'Craft:', person['craft'], '\n' 
+    
+    cords = grab_coordinates()
+    latit, longit = int(float(cords['latitude'])), int(float(cords['longitude']))
+    date_of_passover = indy_data()
+
+    sk = turtle_setup()
+
+    sk.setx(latit)
+    sk.sety(longit)
+
+    turtle.done()
+    return
+
+if __name__ == '__main__':
+    main()
